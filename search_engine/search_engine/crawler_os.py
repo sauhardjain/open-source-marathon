aaddfcfc466404f8491af886911533373fc8c7b6
Sauhard Jain <33095991+sauhardjain@users.noreply.github.com>
2019-07-14 18:33:50 +0530

    basic-crawler-upload

---
+++ search_engine/search_engine/crawler_os.py
@@ -0,0 +1,35 @@
+# web crawler by Sauhard Jain for xino os marathon
+
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+class spider:
+    def __init__(self, url):
+        self.url = url
+
+    def spides(self):
+        r = requests.get(self.url)
+        soup = BeautifulSoup(r.content, 'html5lib')
+
+        if soup.title.string is not None:
+            now = datetime.now()
+            current_time = now.strftime('%H:%M:%S')
+            print('Sir, found title: {} at {}'.format(
+                soup.title.string, current_time))
+
+        for a in soup.find_all('a', href=True):
+            now = datetime.now()
+            access_time = now.strftime('%H:%M:%S')
+            print('Sir, found page: {} at {}'.format(a['href'], current_time))
+
+        for tag in soup.find_all('meta'):
+            now = datetime.now()
+            current_time = now.strftime('%H:%M:%S')
+            if tag.get("property", None) == "ex:title":
+                print('Found meta title: {} at {}'.format(
+                    tag.get("content", None), access_time))
+            if tag.get("property", None) == "ex:description":
+                print('Sir found description: {} at {}\n'.format(
+                    tag.get("content", None), access_time))
